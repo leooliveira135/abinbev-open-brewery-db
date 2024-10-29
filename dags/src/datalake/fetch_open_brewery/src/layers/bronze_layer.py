632039c82a9098fe714865a92595dd4e3d59cecf
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2024-10-29 02:18:42 -0300

    Update bronze_layer.py

--- dags/src/datalake/fetch_open_brewery/src/layers/bronze_layer.py
+++ dags/src/datalake/fetch_open_brewery/src/layers/bronze_layer.py
@@ -1,3 +1,39 @@
+"""
+Bronze Layer - Data Ingestion Script
+
+This module is responsible for fetching raw data from the Open Brewery API and storing it in the Bronze layer of the data lake on AWS S3. The Bronze layer stores data as-is from the source without any transformations, acting as the initial data ingestion point in the data lake architecture.
+
+Functions:
+    - load_data_into_bucket(bucket_name, path, object_data): 
+        Saves JSON data to the specified S3 bucket using the provided path and object data.
+    - fetch_brewery_data(): 
+        Fetches brewery data from the Open Brewery API endpoint and stores it in the Bronze S3 bucket.
+    - main(): 
+        Entry point for initiating the data ingestion process into the Bronze layer.
+
+Modules and Libraries:
+    - requests: Used to perform HTTP requests to the Open Brewery API.
+    - logging: Provides logging capabilities to track the status of operations.
+    - json: For serializing data into JSON format before uploading to S3.
+    - S3Hook: Airflow's AWS S3 hook to facilitate interactions with S3.
+
+Usage:
+    - This script is called by an Apache Airflow DAG as the first step in a multi-layered data pipeline.
+    - It can also be run independently for testing purposes.
+
+Error Handling:
+    - Logs and reports if the API request fails or if the S3 data upload encounters an issue.
+
+Raises:
+    - Exception: If the API request fails with a non-200 status code.
+    - Exception: If data fails to upload to the specified S3 bucket.
+
+Example Workflow:
+    1. The `fetch_brewery_data()` function calls the Open Brewery API to retrieve data.
+    2. If data is successfully retrieved, it is stored in S3 in JSON format via the `load_data_into_bucket()` function.
+    3. Logs are generated to confirm successful data loading or to report any issues.
+"""
+
 import requests
 import logging
 import json
